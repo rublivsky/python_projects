973b6445f4f9af006f523bd33b008b75c093f9ed
Stanislav Rublivskyi <rublivsky@gmail.com>
2024-11-09 22:44:55 +0200

    test3

---
+++ get.py
@@ -0,0 +1,16 @@
+import requests
+
+print("start")
+
+url = 'https://corruptinfo.nazk.gov.ua/ep/1.0/corrupt/getAllData123'
+headers = {'Accept': 'text/plain'}
+
+# response = requests.get(url, headers=headers)
+
+# print("get requests sended")
+
+# print(response.status_code)
+# response_text = response.text
+
+# with open ("test_dp.txt", "w") as file:
+#     file.write(response_text)
